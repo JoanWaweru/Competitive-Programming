156482e28ac4211dd95b3c9abf75d8df74b517d9
JoanWaweru <77121129+JoanWaweru@users.noreply.github.com>
2022-10-24 12:01:58 +0300

    Create fizzBuzz.php

---
+++ Week 1/fizzBuzz.php
@@ -0,0 +1,27 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return String[]
+     */
+    function fizzBuzz(int $n) {
+
+    // PHP program to print Fizz Buzz
+        $number = [];
+        
+        for ($i = 1; $i < $n+1; $i++)
+        {
+            if ($i % 15 == 0){
+                array_push($number,"FizzBuzz");}
+     
+            else if (($i % 3) == 0){
+                array_push($number,"Fizz");}           
+     
+            else if (($i % 5) == 0){                
+                array_push($number,"Buzz");}            
+ 
+            else{         
+                array_push($number,"$i");}          
+        }return $number;
+    }
+}
