dd7fef35a0dbde24960579a3e67ab21d538868ab
JoanWaweru <77121129+JoanWaweru@users.noreply.github.com>
2022-10-24 12:09:49 +0300

    Update fizzBuzz.php

--- Week 1/fizzBuzz.php
+++ Week 1/fizzBuzz.php
@@ -11,7 +11,7 @@ class Solution {
         
         for ($i = 1; $i < $n+1; $i++)
         {
-            if ($i % 15 == 0){
+            if ($i % 5 == 0 && $i == 3 == 0){
                 array_push($number,"FizzBuzz");}
      
             else if (($i % 3) == 0){
